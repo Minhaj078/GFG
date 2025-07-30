4d7d76f0af986de13764c511706e4609f58cb381
Z3N_18 <152924478+Minhaj078@users.noreply.github.com>
2025-07-30 13:06:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Merge Sort/merge-sort.cpp
+++ Difficulty: Medium/Merge Sort/merge-sort.cpp
@@ -1,48 +1,41 @@
 class Solution {
-  public:
-  
-    void merge(vector<int>&arr,int l,int r,int mid){
-        vector<int>temp;
-        
+public:
+    void merge(vector<int>& arr, int l, int mid, int r) {
+        vector<int> temp;
         int left = l;
-        int right = mid+1;
-        while(left<=mid && right <= r){
-            if(arr[left] < arr[right]){
-                temp.push_back(arr[left]);
-                left++;
-            }
-            else{
-                temp.push_back(arr[right]);
-                right++;
+        int right = mid + 1;
+
+        while (left <= mid && right <= r) {
+            if (arr[left] <= arr[right]) {
+                temp.push_back(arr[left++]);
+            } else {
+                temp.push_back(arr[right++]);
             }
         }
-        while(left<=mid){
-            temp.push_back(arr[left]);
-            left++;
+
+        while (left <= mid) {
+            temp.push_back(arr[left++]);
         }
-        
-        while(right<=r){
-            temp.push_back(arr[right]);
-            right++;
+
+        while (right <= r) {
+            temp.push_back(arr[right++]);
         }
-        
-        for(int i = l;i<=r;i++){
-            arr[i] = temp[i-l];
+
+        for (int i = l; i <= r; ++i) {
+            arr[i] = temp[i - l];
         }
     }
-    
-    void mS(vector<int>&arr,int l,int r){
-        if(l>=r)return;
-        
-        int mid = (l+r)/2;
-        
-        mS(arr,l,mid);
-        mS(arr,mid+1,r);
-        merge(arr,l,r,mid);
+
+    void qs(vector<int>& arr, int l, int r) {
+        if (l >= r) return;
+
+        int mid = (l + r) / 2;
+        qs(arr, l, mid);
+        qs(arr, mid + 1, r);
+        merge(arr, l, mid, r);
     }
-    
+
     void mergeSort(vector<int>& arr, int l, int r) {
-        // code here
-        mS(arr,l,r);
+        qs(arr, l, r);
     }
 };
