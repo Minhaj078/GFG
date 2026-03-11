a06dafef31139d11c504409691aa13b3ccb4a343
Minhajuddin Ahmad <152924478+Minhaj078@users.noreply.github.com>
2026-03-11 22:22:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
+++ Difficulty: Easy/Max Sum Subarray of size K/max-sum-subarray-of-size-k.cpp
@@ -3,15 +3,15 @@ class Solution {
     int maxSubarraySum(vector<int>& arr, int k) {
         // code here
         int sum = 0;
-        for(int i = 0;i<k;i++){
+        for(int i = 0; i<k;i++){
             sum += arr[i];
         }
         
         int maxi = sum;
         
-        for(int i = k; i< arr.size();i++){
+        for(int i = k;i<arr.size();i++){
             sum = sum - arr[i-k] + arr[i];
-            maxi = max(maxi, sum);
+            maxi = max(sum,maxi);
         }
         return maxi;
     }
