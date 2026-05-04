9aef8187813496770a1290c66b7efdecb2406ec2
Minhajuddin Ahmad <152924478+Minhaj078@users.noreply.github.com>
2026-05-04 21:55:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Triplets with Smaller Sum/triplets-with-smaller-sum.cpp
+++ Difficulty: Medium/Triplets with Smaller Sum/triplets-with-smaller-sum.cpp
@@ -1,24 +1,20 @@
 class Solution {
-
   public:
-    long long countTriplets(int n, long long sum, long long arr[]) {
-        // Your code goes here
-        
-        sort(arr,arr + n);
-        long long cnt = 0;
-        
-        for(int i = 0; i<n-2; i++){
-            int left = i + 1, right = n - 1;
-            
-            while(left < right){
-                long long target = arr[i] + arr[left] + arr[right];
-                
-                if(target < sum){
-                    cnt += (right - left);
-                    left++;
+    int countTriplets(int sum, vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        int cnt = 0;
+        for(int i = 0;i<n-2;i++){
+            int l = i+1, r = n-1;
+            while(l < r ){
+                long long t = arr[i] + arr[l] + arr[r];
+                if(t < sum){
+                    cnt += (r - l);
+                    l++;
                 }
-                else {
-                    right--;
+                else{
+                    r--;
                 }
             }
         }
